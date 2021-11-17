f0d5a67c03cded2ea5dfd5c5d04b561b613ae277
nahtlinh <94429889+nahtlinh@users.noreply.github.com>
2021-11-17 13:58:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ function firstQuestion() {
         imageUrl: 'img/logi.gif',
         imageWidth: 300,
         imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
+        background: '#fff url("img/iput-theme.jpg")',
         imageAlt: 'Custom image',
         confirmButtonText: CONFIG.btnIntro
     }).then(function() {
